915d927ba55db1783cc060576e093ebf56c11d34
byteAndBait <abdulrahman.elkassas@yahoo.com>
2025-03-08 17:34:12 +0200

    Fix the bugs and add reseting the game

--- script.js
+++ script.js
@@ -81,7 +81,8 @@ let computerChoice;
 let playerChoice;
 let computerChoiceElement = document.querySelector(".computerChoice");
 let playerChoiceElement = document.querySelector(".playerChoice");
-
+let resetButton = document.querySelector(".reset");
+let roundCountElement = document.querySelector(".roundCountElement")
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   switch (randomNumber) {
@@ -98,45 +99,80 @@ buttons.forEach(ele => {
   ele.addEventListener("click", () => {
     playerChoice = ele.textContent;
     computerChoice = getComputerChoice();
+    statusElement.innerHTML = "";
     playRound(computerChoice, playerChoice);
   });
 });
 
 function playRound(computer, player) {
-  if (!(roundCount <= 5)) {
+  roundCountElement.innerHTML = `
+  round: ${roundCount}
+  `
+  // check the round and end it if it's the fifth round and review the scores
+  if (roundCount == 5) {
+    if (playerScore > computerScore) {
+      statusElement.innerHTML = `player won the game with the a score of [${playerScore}]`;
+    } else if (computerScore > playerScore) {
+      statusElement.innerHTML = `computer won the game with a score of [${computerScore}]`;
+    } else {
+      statusElement.innerHTML = `Tie`;
+    }
+
+    // disable the choice buttons to make the player unable to do changes to the final result
+    buttons.forEach(ele => {
+      ele.disabled = true;
+    });
+    // reseting scores
+    computerScore = 0;
+    playerScore = 0;
     return;
   }
-  computerChoiceElement.innerHTML = `computer chose ${computer}`;
-  playerChoiceElement.innerHTML = `player chose ${player}`;
+  // beginning of the round
+  roundCount++;
+  computerChoiceElement.innerHTML = `computer chose <span>${computer}</span>`;
+  playerChoiceElement.innerHTML = `player chose <span>${player}</span>`;
+
+  // in case of tie
   if (player == computer) {
-    // in case of tie
-    statusElement.innerHTML = "Tie";
+    computerChoice = getComputerChoice();
+    statusElement.innerHTML = "This round is Tie";
     return;
   }
+  function computerWins() {
+    computerScore++;
+    computerScoreElement.innerHTML = `computer score is: [${computerScore}]`;
+    statusElement.innerHTML = "Computer won this round"
+
+  }
+  function playerWins() {
+    playerScore++;
+    playerScoreElement.innerHTML = `player score is: [${playerScore}]`;
+    statusElement.innerHTML = "Player won this round"
+  }
+
   // cases
   if (computer == "rock") {
     if (player == "scissors") {
-      computerScore++;
-      computerScoreElement.innerHTML = `computer score is: [${computerScore}]`;
+      computerWins();
     } else if (player == "paper") {
-      playerScore++;
-      playerScoreElement.innerHTML = `player score is: [${playerScore}]`;
+      playerWins();
     }
   } else if (computer == "scissors") {
     if (player == "paper") {
-      computerScore++;
-      computerScoreElement.innerHTML = `computer score is: [${computerScore}]`;
+      computerWins();
     } else if (player == "rock") {
-      playerScore++;
-      playerScoreElement.innerHTML = `player score is: [${playerScore}]`;
+      playerWins();
     }
   } else if (computer == "paper") {
     if (player == "rock") {
-      computerScore++;
-      computerScoreElement.innerHTML = `computer score is: [${computerScore}]`;
+      computerWins();
     } else if (player == "scissors") {
-      playerScore++;
-      playerScoreElement.innerHTML = `player score is: [${playerScore}]`;
+      playerWins();
     }
   }
 }
+
+
+resetButton.addEventListener("click", () =>{
+    location.reload()
+})
