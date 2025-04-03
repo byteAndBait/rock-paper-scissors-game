fc4ce9d9d9ef5437d24901f38bb735ccf628bfc0
byteAndBait <abdulrahman.elkassas@yahoo.com>
2025-04-03 14:42:23 +0200

    Add event delegation to improve performance

--- script.js
+++ script.js
@@ -1,75 +1,3 @@
-/*
-input would be rock paper or scissors from both the human and computer
-output is who won
-the game has 5 rounds and a score tracker
-whoever get a higher score wins
-Init the choice element reveal for both of them
-Init the score element for computer and user
-Init the status element
-Init roundCount variable
-Init a function to get the computer choice getComputerChoice()
-    generate a random number between 1 and 3
-    reference rock scissors and paper using the numbers
-    cases paper 1 , scissors 2, rock 3
-    Return the string
-
-Init a variable for the buttons on the page
-Init a function to get the player choice getPlayerChoice()
-    Return the button textcontent
-
-Init a function to play the round playRound(player,computer )
-    check the round count first if less or equal to 5
-    otherwise
-    use 3 switch cases for who won
-
-    write the choice of the player to the player reveal element 
-    write the choice of the computer to the computer reveal element 
-
-    if player = computer 
-    status element is "Tie"
-        return 
-
-
-    if computer is rock
-        if player is paper
-            player score increase by 1
-                status element is "player scores"
-        if player is scissors
-            computer score incerase by 1
-                status element is "computer scores"
-
-    if computer is paper
-        if player is rock
-            computer score increase by 1
-                status element is "computer scores"
-
-        if player is scissors
-            player score incerase by 1
-                status element is "player scores"
-
-    if computer is scissors
-        if player is paper
-            computer score increase by 1
-        if player is rock
-            player score incerase by 1
-                status element is "player scores"
-
-    increase the round count by 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
 let computerScore = 0;
 let playerScore = 0;
 let computerScoreElement = document.querySelector(".computerScore");
@@ -82,7 +10,8 @@ let playerChoice;
 let computerChoiceElement = document.querySelector(".computerChoice");
 let playerChoiceElement = document.querySelector(".playerChoice");
 let resetButton = document.querySelector(".reset");
-let roundCountElement = document.querySelector(".roundCountElement")
+let roundCountElement = document.querySelector(".roundCountElement");
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   switch (randomNumber) {
@@ -95,19 +24,19 @@ function getComputerChoice() {
   }
 }
 
-buttons.forEach(ele => {
-  ele.addEventListener("click", () => {
-    playerChoice = ele.textContent;
+document.querySelector(".choices").addEventListener("click", e => {
+  if (e.target.classList.contains("choice")) {
+    playerChoice = e.target.textContent;
     computerChoice = getComputerChoice();
     statusElement.innerHTML = "";
     playRound(computerChoice, playerChoice);
-  });
+  }
 });
 
 function playRound(computer, player) {
   roundCountElement.innerHTML = `
   round: ${roundCount}
-  `
+  `;
   // check the round and end it if it's the fifth round and review the scores
   if (roundCount == 5) {
     if (playerScore > computerScore) {
@@ -141,13 +70,12 @@ function playRound(computer, player) {
   function computerWins() {
     computerScore++;
     computerScoreElement.innerHTML = `computer score is: [${computerScore}]`;
-    statusElement.innerHTML = "Computer won this round"
-
+    statusElement.innerHTML = "Computer won this round";
   }
   function playerWins() {
     playerScore++;
     playerScoreElement.innerHTML = `player score is: [${playerScore}]`;
-    statusElement.innerHTML = "Player won this round"
+    statusElement.innerHTML = "Player won this round";
   }
 
   // cases
@@ -172,7 +100,6 @@ function playRound(computer, player) {
   }
 }
 
-
-resetButton.addEventListener("click", () =>{
-    location.reload()
-})
+resetButton.addEventListener("click", () => {
+  location.reload();
+});
